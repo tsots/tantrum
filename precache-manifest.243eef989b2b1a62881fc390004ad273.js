bebd98049f578edf585e19a38eabe6b4bdb6f124
tsots <mosalatsotetsi@gmail.com>
2020-01-18 17:38:33 +0200

    Updates

---
+++ precache-manifest.243eef989b2b1a62881fc390004ad273.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "631e9140641c205f0dfef340605499ff",
+    "url": "/tantrum/index.html"
+  },
+  {
+    "revision": "d3c02b92f2d0a997e168",
+    "url": "/tantrum/static/css/main.5b7333d5.chunk.css"
+  },
+  {
+    "revision": "e0d97b6e91d2ab60fd4a",
+    "url": "/tantrum/static/js/2.d286dd92.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/tantrum/static/js/2.d286dd92.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d3c02b92f2d0a997e168",
+    "url": "/tantrum/static/js/main.713e7d0b.chunk.js"
+  },
+  {
+    "revision": "a525b12c619b015ad4f6",
+    "url": "/tantrum/static/js/runtime-main.40ff087b.js"
+  },
+  {
+    "revision": "509bd449ed36082729b972ea8089e1ce",
+    "url": "/tantrum/static/media/Tsotetsi-Mosala.509bd449.png"
+  },
+  {
+    "revision": "a1c4174fe06e1d763a3e12223e722bd5",
+    "url": "/tantrum/static/media/UI-design-large.a1c4174f.svg"
+  },
+  {
+    "revision": "4f668c3b21faa5bfb604789a23c5e32d",
+    "url": "/tantrum/static/media/UI-design-small.4f668c3b.svg"
+  },
+  {
+    "revision": "13ce2375d8fe9a459317a947533e1e4b",
+    "url": "/tantrum/static/media/UX-design-large.13ce2375.svg"
+  },
+  {
+    "revision": "c61d779ecd97f2f915b0c193a975a323",
+    "url": "/tantrum/static/media/UX-design-small.c61d779e.svg"
+  },
+  {
+    "revision": "49fee6d915ccd405d3c936bcd5f2e352",
+    "url": "/tantrum/static/media/branding-strategy-large.49fee6d9.svg"
+  },
+  {
+    "revision": "7e9bb461ecb385bd6f676fdbadb4e856",
+    "url": "/tantrum/static/media/branding-strategy-small.7e9bb461.svg"
+  },
+  {
+    "revision": "c311466f9e0994102ff977937be8a242",
+    "url": "/tantrum/static/media/collage-large-1.c311466f.png"
+  },
+  {
+    "revision": "2bac3103031ae9b4a80e5b4a2cd1ded7",
+    "url": "/tantrum/static/media/corporate-branding-large.2bac3103.svg"
+  },
+  {
+    "revision": "7b27d7c73ff428fb3214c8ff0941c282",
+    "url": "/tantrum/static/media/corporate-branding-small.7b27d7c7.svg"
+  },
+  {
+    "revision": "2552aa06c7d0734564ae5a09f8cf2f12",
+    "url": "/tantrum/static/media/digital-design-large.2552aa06.svg"
+  },
+  {
+    "revision": "543e279e685adab9f3f31fe9b5292889",
+    "url": "/tantrum/static/media/digital-design-small.543e279e.svg"
+  },
+  {
+    "revision": "c0f8e0eddb30595c383c4d6b44fd5290",
+    "url": "/tantrum/static/media/logo-design-large.c0f8e0ed.svg"
+  },
+  {
+    "revision": "74a1e47c4ba8d5590c035ca6849e234f",
+    "url": "/tantrum/static/media/logo-design-small.74a1e47c.svg"
+  },
+  {
+    "revision": "e037c2e8d037d6ebb8859f24e91338fd",
+    "url": "/tantrum/static/media/logo-large.e037c2e8.svg"
+  },
+  {
+    "revision": "380cea457104c6ea00fe8ae00c236ff2",
+    "url": "/tantrum/static/media/marketing-assets-large.380cea45.svg"
+  },
+  {
+    "revision": "9445670bb130daae9a3b0bca7b4a7249",
+    "url": "/tantrum/static/media/marketing-assets-small.9445670b.svg"
+  },
+  {
+    "revision": "fb463d857463cee7d9c8291522a59dc3",
+    "url": "/tantrum/static/media/messaging-large.fb463d85.svg"
+  },
+  {
+    "revision": "3da14e507852be22b956714be410154c",
+    "url": "/tantrum/static/media/messaging-small.3da14e50.svg"
+  },
+  {
+    "revision": "dc897d28fe5b835ada2f96178a20aeb2",
+    "url": "/tantrum/static/media/personal-branding-large.dc897d28.svg"
+  },
+  {
+    "revision": "5074d32595085f1b702d0b0d55f7a008",
+    "url": "/tantrum/static/media/personal-branding-small.5074d325.svg"
+  },
+  {
+    "revision": "4befca1b1d7df4371ab20009cf0fba5b",
+    "url": "/tantrum/static/media/research-large.4befca1b.svg"
+  },
+  {
+    "revision": "f8bd0a4270215802761e6f6945f00af8",
+    "url": "/tantrum/static/media/research-small.f8bd0a42.svg"
+  },
+  {
+    "revision": "bfb03f788963ac30183a8c4df255d2e2",
+    "url": "/tantrum/static/media/responsive-web-large.bfb03f78.svg"
+  },
+  {
+    "revision": "5387258b2bad8a22462539c084fdb72d",
+    "url": "/tantrum/static/media/responsive-web-small.5387258b.svg"
+  },
+  {
+    "revision": "3587c5d942d78e7528e408f563245542",
+    "url": "/tantrum/static/media/social-media-large.3587c5d9.svg"
+  },
+  {
+    "revision": "76ce5792624a6861df81f62b44432e18",
+    "url": "/tantrum/static/media/social-media-small.76ce5792.svg"
+  },
+  {
+    "revision": "2a87dd0aca9c4a1e2497e6586c13e4d2",
+    "url": "/tantrum/static/media/web-design-large.2a87dd0a.svg"
+  },
+  {
+    "revision": "74debbf2cf90c8c859c684d7ca02557a",
+    "url": "/tantrum/static/media/web-design-small.74debbf2.svg"
+  }
+]);
